d6d3fec50b23bef10088d29c38463a8d85e0cb53
Jonas de Bruin <jonasdebruin05@gmail.com>
2024-03-22 16:49:42 +0100

    Fixed build.bat

--- build.bat
+++ build.bat
@@ -3,15 +3,11 @@
 taskkill /im Flow.Launcher.exe
 pause 
 set "sourceFolder=.\FandomSearch\Flow.Launcher.Plugin.FandomSearch\bin\Debug"
-set "destinationFolder=C:\Users\jonas\AppData\Local\FlowLauncher\app-1.17.2\UserData\Plugins\FandomSearch-DEV\"
+set "destinationFolder=C:\Users\jonas\AppData\Roaming\FlowLauncher\Plugins\FandomSearch-DEV\"
 
 echo Copying files from %sourceFolder% to %destinationFolder%...
 
 xcopy /E /I /Y "%sourceFolder%" "%destinationFolder%"
 
 echo Copy completed.
-
-echo Starting Flow Launcher...
-start "" "C:\Users\jonas\AppData\Local\FlowLauncher\Flow.Launcher.exe"
-
 pause
