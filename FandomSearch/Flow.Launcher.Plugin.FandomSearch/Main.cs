158389019a270bc3112d87fefdc32287a2c7158c
Jonas de Bruin <jonasdebruin05@gmail.com>
2024-04-04 13:00:24 +0200

    Fixed reading from json not working

--- FandomSearch/Flow.Launcher.Plugin.FandomSearch/Main.cs
+++ FandomSearch/Flow.Launcher.Plugin.FandomSearch/Main.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using Newtonsoft.Json;
 using System.IO;
 using System;
+using System.Reflection;
 
 
 namespace Flow.Launcher.Plugin.FandomSearch
@@ -27,6 +28,10 @@ namespace Flow.Launcher.Plugin.FandomSearch
         private string jsonResult;
         private string finalUrl;
 
+        // Path to config file
+        public static string AssemblyDirectory { get; } = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        public static string DataDirectory { get; } = Path.Combine(AssemblyDirectory, @"..\..\");
+
         // Path to icon
         private readonly string icon_path = "icon.png";
 
@@ -40,59 +45,71 @@ namespace Flow.Launcher.Plugin.FandomSearch
 
         public async Task<List<Result>> QueryAsync(Query query, CancellationToken token)
         {
-            // Make request to terraria wiki api with the query the user has put in
-            using (var httpClient = new HttpClient())
+            var results = new List<Result>();
+
+            results.Add(new Result
             {
-                jsonResult = await httpClient.GetStringAsync(query_url + query.Search);
-            }
+                Title = actionKeywords[0]
+            });
 
-            // Store the daya
-            dynamic data = JObject.Parse(jsonResult);
-            // Also store as a JObject so we can check if it contains the error key
-            JObject dataObj = JObject.Parse(jsonResult);
+            return await Task.FromResult(results);
+        } 
 
-            // Check if the data has error key (happens when the user has not given an input yet)
-            if (dataObj.ContainsKey("error"))
-            {
-                // Make simple result 
-                var noResults = new List<Result>
-                {
-                    new Result
-                    {
-                        Title = $"Start typing to search the wiki...",
-                        IcoPath = "icon.png"
-                    }
-                };
-                return await Task.FromResult(noResults);
-            }
-            else
-            {
-                var results = new List<Result>();
+       // public async Task<List<Result>> QueryAsync(Query query, CancellationToken token)
+       // {
+       //     // Make request to terraria wiki api with the query the user has put in
+       //     using (var httpClient = new HttpClient())
+       //     {
+       //         jsonResult = await httpClient.GetStringAsync(query_url + query.Search);
+       //     }
 
-                // Loop over all the results and make a result item for them all
-                foreach (var item in data.query.search)
-                {
-                    string itemStr = item.title;
-                    string itemWithunderscores = itemStr.Replace(" ", "_");
+       //     // Store the daya
+       //     dynamic data = JObject.Parse(jsonResult);
+       //     // Also store as a JObject so we can check if it contains the error key
+       //     JObject dataObj = JObject.Parse(jsonResult);
 
-                    results.Add(new Result
-                    {
-                        Title = $"{item.title}",
-                        SubTitle = $"https://terraria.fandom.com/wiki/" + itemWithunderscores,
-                        Action = e =>
-                        {
-                            // Make final url to search
-                            finalUrl =  "wiki/" + itemWithunderscores;
-                            finalUrl.OpenInBrowserTab();
-                            return true;
-                        },
-                        IcoPath = "icon.png"
-                    });
-                }
-                // Return the results
-                return await Task.FromResult(results);
-            }
-        }
+       //     // Check if the data has error key (happens when the user has not given an input yet)
+       //     if (dataObj.ContainsKey("error"))
+       //     {
+       //         // Make simple result 
+       //         var noResults = new List<Result>
+       //         {
+       //             new Result
+       //             {
+       //                 Title = $"Start typing to search the wiki...",
+       //                 IcoPath = "icon.png"
+       //             }
+       //         };
+       //         return await Task.FromResult(noResults);
+       //     }
+       //     else
+       //     {
+       //         var results = new List<Result>();
+
+       //         // Loop over all the results and make a result item for them all
+       //         foreach (var item in data.query.search)
+       //         {
+       //             string itemStr = item.title;
+       //             string itemWithunderscores = itemStr.Replace(" ", "_");
+
+       //             results.Add(new Result
+       //             {
+       //                 Title = $"{item.title}",
+       //                 SubTitle = $"https://terraria.fandom.com/wiki/" + itemWithunderscores,
+       //                 Action = e =>
+       //                 {
+       //                     // Make final url to search
+       //                     finalUrl =  "wiki/" + itemWithunderscores;
+       //                     finalUrl.OpenInBrowserTab();
+       //                     return true;
+       //                 },
+       //                 IcoPath = "icon.png"
+       //             });
+       //         }
+       //         // Return the results
+       //         return await Task.FromResult(results);
+       //     }
+       // }
 
         public Control CreateSettingPanel() => new SettingsControl(_settings);
 
@@ -113,9 +130,10 @@ namespace Flow.Launcher.Plugin.FandomSearch
             actionKeywords.Clear();
 
             // Get the path to the settings file
-            string fullPath = Path.Combine(Environment.CurrentDirectory, "settings.json");
+            string fullPath = Path.Combine(Environment.CurrentDirectory, "fandomSettings.json");
+            string path = DataDirectory + @"Settings\fandomSettings.json";
             
-            JObject jsonData = JObject.Parse(File.ReadAllText(fullPath));
+            JObject jsonData = JObject.Parse(File.ReadAllText(path));
             JArray fandoms = (JArray)jsonData["Fandoms"];
 
             foreach (JObject fandom in fandoms)
