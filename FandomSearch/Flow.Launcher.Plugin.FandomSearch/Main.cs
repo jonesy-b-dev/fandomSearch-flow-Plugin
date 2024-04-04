0bd4c7b8f46eb1951a1afce0604d3efeafbe9bde
Jonas de Bruin <jonasdebruin05@gmail.com>
2024-04-04 14:32:20 +0200

    Check what fandom is being inputted (untested)

--- FandomSearch/Flow.Launcher.Plugin.FandomSearch/Main.cs
+++ FandomSearch/Flow.Launcher.Plugin.FandomSearch/Main.cs
@@ -27,6 +27,7 @@ namespace Flow.Launcher.Plugin.FandomSearch
         private string query_url = "ben";
         private string jsonResult;
         private string finalUrl;
+        private string formattedInput;
 
         // Path to config file
         public static string AssemblyDirectory { get; } = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
@@ -43,73 +44,91 @@ namespace Flow.Launcher.Plugin.FandomSearch
             _context = context;
         }
 
-        public async Task<List<Result>> QueryAsync(Query query, CancellationToken token)
-        {
-            var results = new List<Result>();
-
-            results.Add(new Result
-            {
-                Title = actionKeywords[0]
-            });
-
-            return await Task.FromResult(results);
-        } 
-
        // public async Task<List<Result>> QueryAsync(Query query, CancellationToken token)
        // {
-       //     // Make request to terraria wiki api with the query the user has put in
-       //     using (var httpClient = new HttpClient())
+       //     var results = new List<Result>();
+
+       //     results.Add(new Result
        //     {
-       //         jsonResult = await httpClient.GetStringAsync(query_url + query.Search);
-       //     }
+       //         Title = actionKeywords[0]
+       //     });
 
-       //     // Store the daya
-       //     dynamic data = JObject.Parse(jsonResult);
-       //     // Also store as a JObject so we can check if it contains the error key
-       //     JObject dataObj = JObject.Parse(jsonResult);
+       //     return await Task.FromResult(results);
+       // } 
 
-       //     // Check if the data has error key (happens when the user has not given an input yet)
-       //     if (dataObj.ContainsKey("error"))
-       //     {
-       //         // Make simple result 
-       //         var noResults = new List<Result>
-       //         {
-       //             new Result
-       //             {
-       //                 Title = $"Start typing to search the wiki...",
-       //                 IcoPath = "icon.png"
-       //             }
-       //         };
-       //         return await Task.FromResult(noResults);
-       //     }
-       //     else
-       //     {
-       //         var results = new List<Result>();
+        public async Task<List<Result>> QueryAsync(Query query, CancellationToken token)
+        {
+            string selectedFandom;
 
-       //         // Loop over all the results and make a result item for them all
-       //         foreach (var item in data.query.search)
-       //         {
-       //             string itemStr = item.title;
-       //             string itemWithunderscores = itemStr.Replace(" ", "_");
+            if(query.Search.Contains(" "))
+            {
+                formattedInput = query.Search.Substring(0, query.Search.IndexOf(" "));
+            }
+            else
+            {
+                formattedInput = query.Search;
+            }
 
-       //             results.Add(new Result
-       //             {
-       //                 Title = $"{item.title}",
-       //                 SubTitle = $"https://terraria.fandom.com/wiki/" + itemWithunderscores,
-       //                 Action = e =>
-       //                 {
-       //                     // Make final url to search
-       //                     finalUrl =  "wiki/" + itemWithunderscores;
-       //                     finalUrl.OpenInBrowserTab();
-       //                     return true;
-       //                 },
-       //                 IcoPath = "icon.png"
-       //             });
-       //         }
-       //         // Return the results
-       //         return await Task.FromResult(results);
-       //     }
-       // }
+            foreach (var actionKeyword in actionKeywords)
+            {
+                if(formattedInput.Contains(actionKeyword))
+                {
+                    selectedFandom = actionKeyword;
+                }
+            }
+            // Make request to terraria wiki api with the query the user has put in
+             using (var httpClient = new HttpClient())
+             {
+                jsonResult = await httpClient.GetStringAsync(query_url + query.Search);
+             }
+
+            // Store the daya
+            dynamic data = JObject.Parse(jsonResult);
+            // Also store as a JObject so we can check if it contains the error key
+            JObject dataObj = JObject.Parse(jsonResult);
+
+            // Check if the data has error key (happens when the user has not given an input yet)
+            if (dataObj.ContainsKey("error"))
+            {
+                // Make simple result 
+                var noResults = new List<Result>
+                {
+                    new Result
+                    {
+                        Title = $"Start typing to search the wiki...",
+                        IcoPath = "icon.png"
+                    }
+                };
+                return await Task.FromResult(noResults);
+            }
+            else
+            {
+                var results = new List<Result>();
+
+                // Loop over all the results and make a result item for them all
+                foreach (var item in data.query.search)
+                {
+                    string itemStr = item.title;
+                    string itemWithunderscores = itemStr.Replace(" ", "_");
+
+                    results.Add(new Result
+                    {
+                        Title = $"{item.title}",
+                        SubTitle = $"https://terraria.fandom.com/wiki/" + itemWithunderscores,
+                        Action = e =>
+                        {
+                            // Make final url to search
+                            finalUrl =  "wiki/" + itemWithunderscores;
+                            finalUrl.OpenInBrowserTab();
+                            return true;
+                        },
+                        IcoPath = "icon.png"
+                    });
+                }
+                // Return the results
+                return await Task.FromResult(results);
+            }
+        }
 
         public Control CreateSettingPanel() => new SettingsControl(_settings);
 
